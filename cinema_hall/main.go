97d35da077030d2738ba0a520a7f60189e3fac3c
hmkrivoj <krivoj@hm.edu>
2019-06-22 22:25:29 +0200

    :sparkles: create cinema hall

--- cinema_hall/main.go
+++ cinema_hall/main.go
@@ -5,13 +5,44 @@ import (
 	"github.com/micro/go-log"
 	"github.com/micro/go-micro"
 	proto "github.com/ob-vss-ss19/blatt-4-forever_alone_2_electric_boogaloo/cinema_hall/proto"
+	"sync"
 )
 
 type CinemaHallHandler struct {
+	mutex       sync.Mutex
+	idCounter   int64
+	cinemaHalls map[int64]cinemaHall
 }
 
-func (*CinemaHallHandler) Create(context.Context, *proto.CreateRequest, *proto.CreateResponse) error {
-	panic("implement me")
+type cinemaHall struct {
+	id   int64
+	name string
+	rows int32
+	cols int32
+}
+
+func (handler *CinemaHallHandler) Create(ctx context.Context, req *proto.CreateRequest, res *proto.CreateResponse) error {
+	handler.mutex.Lock()
+	id := handler.idCounter
+	handler.idCounter++
+	handler.mutex.Unlock()
+
+	hall := cinemaHall{
+		id:   id,
+		name: req.Name,
+		rows: req.Rows,
+		cols: req.Cols,
+	}
+	handler.cinemaHalls[id] = hall
+
+	res.Hall = &proto.CinemaHall{
+		Id:   hall.id,
+		Name: hall.name,
+		Rows: hall.rows,
+		Cols: hall.cols,
+	}
+
+	return nil
 }
 
 func (*CinemaHallHandler) Delete(context.Context, *proto.DeleteRequest, *proto.DeleteResponse) error {
